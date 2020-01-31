aed1864481fdca9be492a2ef3cf42bccd3b05ce5
fishmanjohn <john@emmaandjohnbyvan.com>
2020-01-31 08:55:40 -0800

    auth done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,50 @@
 const router = require('express').Router();
+const Users = require('./users-model');
+const bc = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+
+  const hash = bc.hashSync(req.body.password, 3);
+  user.password = hash;
+
+  Users.add(user)
+      .then(saved => {
+          res.status(201).json(saved);
+      })
+      .catch(error => {
+          res.status(500).json(error);
+      });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bc.compareSync(password, user.password)) {
+                const token = generateToken(user)
+                res.status(200).json({ message: `Welcome ${user.username}!`, token});
+            } else {
+                res.status(401).json({ message: "Invalid Credentials" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
+
+function generateToken(user){
+  const payload = {
+      username: user.username,
+      id: user.id
+  }
+  const options ={
+      expiresIn: '1d'
+  }
+  return jwt.sign(payload, process.env.JWT_SECRET || 'StewartsMassiveHorn', options)
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,21 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken')
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+    if(token){
+       jwt.verify(token,process.env.JWT_SECRET || 'StewartsMassiveHorn', (err, decodedToken)=>{
+           if(err){
+            res.status(401).json({maessage:'Your token was as bad as these jokes that you will not see...'})
+           }else{
+               req.user = decodedToken;
+               next()
+           }
+       })
+    }else{
+      res.status(401).json({ you: 'shall not pass!' });
+    }
+  
 };
